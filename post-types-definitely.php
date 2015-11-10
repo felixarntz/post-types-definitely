4ffd2b6dba3a45f009efdab6c79b1d21d0df678b
Felix Arntz <felix-arntz@t-online.de>
2015-11-10 01:23:33 +0100

    fixed license

--- post-types-definitely.php
+++ post-types-definitely.php
@@ -6,8 +6,8 @@ Description: This framework plugin makes adding post types with taxonomies and m
 Version: 0.5.0
 Author: Felix Arntz
 Author URI: http://leaves-and-love.net
-License: GNU General Public License v2
-License URI: http://www.gnu.org/licenses/gpl-2.0.html
+License: GNU General Public License v3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
 Text Domain: post-types-definitely
 Domain Path: /languages/
 Tags: wordpress, plugin, framework, library, developer, post-types, taxonomies, meta, admin, backend, ui
